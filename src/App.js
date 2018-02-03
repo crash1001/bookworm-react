6b810d9334a5f042c55f2f107308e87d4514beef
Ditish Maharjan <ditish.maharjan@rts.com.np>
2018-02-03 14:48:00 +0545

    Login form with semantic UI, username,password printed on console basic react,state passing, route

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// Library Imports
+import React from "react";
+import { Route } from "react-router-dom";
+
+// File Imports
+import HomePage from "./components/pages/HomePage";
+import LoginPage from "./components/pages/LoginPage";
+
+const App = () => (
+  <div className="ui container">
+    <Route path="/" exact component={HomePage} />
+    <Route path="/login" exact component={LoginPage} />
+  </div>
+);
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,17 @@
+// Library Imports
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "semantic-ui-css/semantic.min.css";
+
+// File Imports
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
+registerServiceWorker();
